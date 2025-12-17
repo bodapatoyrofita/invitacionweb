7b15630d3a952f0ae2586bd79a026c574e89f018
Pablo <pdrichieri@gmail.com>
2025-12-17 19:41:55 -0300

    gift test nto finished

--- utils.js
+++ utils.js
@@ -17,3 +17,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
   elements.forEach(el => observer.observe(el));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll("#regalo-left");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("pop-from-left");
+          observer.unobserve(entry.target); // animate once
+        }
+      });
+    },
+    {
+      threshold: 0.2 // 20% visible before triggering
+    }
+  );
+
+  elements.forEach(el => observer.observe(el));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll("#regalo-right");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("pop-from-right");
+          observer.unobserve(entry.target); // animate once
+        }
+      });
+    },
+    {
+      threshold: 0.2 // 20% visible before triggering
+    }
+  );
+
+  elements.forEach(el => observer.observe(el));
+});
